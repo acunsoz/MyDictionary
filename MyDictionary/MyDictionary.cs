2e457b38413799a5217a15ea0071bce6810e69ee
ALIHAN <ALIHAN@DESKTOP-52972LB>
2021-02-21 17:34:16 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    public class MyDictionary<T,A>
+    {
+        T[] _karray;//key
+        A[] _varray;//value
+        T[] _tempkarray;//gecici key
+        A[] _tempvarray;//gecici value
+        public MyDictionary()
+        {
+            _karray = new T[0];
+            _varray = new A[0];
+        }
+        public void Add(T item1,A item2)
+        {
+            _tempkarray = _karray;
+            _tempvarray = _varray;
+
+            _karray = new T[_karray.Length+1];
+            _varray = new A[_varray.Length + 1];
+
+            for (int i = 0; i < _tempkarray.Length; i++)
+            {
+                _karray[i] = _tempkarray[i];
+                _varray[i] = _tempvarray[i];
+            }
+            _karray[_karray.Length - 1] = item1;
+            _varray[_varray.Length - 1] = item2;
+
+            Console.WriteLine(_karray.Length+" adet ekleme yapıldı "+"{"+ _karray[_karray.Length-1]+" , "+ _varray[_varray.Length - 1]+"}");
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, int> islem = new MyDictionary<string, int>();
+            islem.Add("Ali",12);
+            islem.Add("Mehmet",13);
+            islem.Add("Kerem", 16);
+            islem.Add("Aslı", 17);
+
+        }
+        
+    }
+}
